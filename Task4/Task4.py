8d270eab7ad01b82c8e5f2aad0f7ea56bec88e68
Dmitrii Mironov <dimamironov@mail.ru>
2022-11-28 20:30:26 +0300

    Task 4 completed

--- Task4/Task4.py
+++ Task4/Task4.py
@@ -7,8 +7,32 @@ import os
 
 os.system('cls')
 
-print('Проверим является ли день выходным.')
+print('Найдем произведение чисел ряда по заранее заданным позициям. Позиции заданы в файле positions.txt.')
 
-a = int(input('Введите цифру соответствующую дню недели: '))
+n = int(input('Введите число N (определяет промежуток [-N, N]): '))
 
+elements = []
+result = 1
 
+for i in range(-n, n + 1):
+    elements.append(i)
+i = 0
+
+print(f'Получился такой ряд чисел: {elements}.')
+print()
+
+InputPositions = input('Введите позиции чисел ряда, которые хотите перемножить (они будут внесены в файл positions.txt): ').split()
+
+with open('positions.txt', 'w') as file:
+    file.write('\n'.join(InputPositions))
+
+with open('positions.txt', 'r') as file:
+    ReadPositions = list(map(int, file.read().splitlines()))
+
+while i < len(ReadPositions):
+    index = ReadPositions[i]
+    result = result * elements[index]
+    i += 1
+
+print()
+print(f'Произведение элементов ряда {elements} на позициях {ReadPositions} равно: {result}.')
